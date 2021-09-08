40980dfe6bccbce3577a2305958ec6d9a789f345
Maximo-4ever <choripapa04@gmail.com>
2021-09-08 11:05:27 -0500

    Implementacion de busqueda por dominio

--- main.js
+++ main.js
@@ -1,52 +1,55 @@
 // Formulario
 const form = document.querySelector(".form");
-const input = document.querySelector(".form__input");
+let input = document.querySelector(".form__input");
 // Informacion
-const ip = document.getElementById("ip");
+const ipParagraph = document.getElementById("ip");
 const locations = document.getElementById("location");
 const timezone = document.getElementById("timezone");
 const isp = document.getElementById("isp");
 // Map
+const titlesProvider = "https://tile.openstreetmap.org/{z}/{x}/{y}.png";
 var lat;
 var lng;
-const titlesProvider = "https://tile.openstreetmap.org/{z}/{x}/{y}.png";
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  let ipInput = input.value;
-  if (ipInput !== "" && ipInput !== undefined) {
-    //console.log("HI");
-    pedirInfo(ipInput);
-  }
-  else {
+  inputIP = input.value;
+  if (inputIP !== "" && inputIP !== undefined) {
+    pedirInfo(inputIP);
+  } else {
     alert("Please enter a valid IP address");
   }
 });
-const pedirInfo = async (ipInput) => {
+const pedirInfo = async (ip) => {
   try {
-    const secretAPI = await fetch(
-      `https://geo.ipify.org/api/v1?apiKey=at_oXuQi6cvttxp33vuNOMfhq3pHjbVa&ipAddress=${ipInput}`
-    );
-    data = await secretAPI.json();
-    pintarInfo(data);
-
-    lat = data.location.lat;
-    lng = data.location.lng;
-    actualizarMapa();
+    if (inputIP.includes("com")) {
+      let secretAPI = await fetch(
+        `https://geo.ipify.org/api/v1?apiKey=at_oXuQi6cvttxp33vuNOMfhq3pHjbVa&domain=${ip}`
+      );
+      data = await secretAPI.json();
+      pintarInfo(data);
+    } else {
+      let secretAPI = await fetch(
+        `https://geo.ipify.org/api/v1?apiKey=at_oXuQi6cvttxp33vuNOMfhq3pHjbVa&ipAddress=${ip}`
+      );
+      data = await secretAPI.json();
+      pintarInfo(data);
+    }
+    actualizarMapa(data);
   } catch (error) {
     console.log(error);
     alert("Please enter a valid IP address");
   }
 };
 const pintarInfo = (data) => {
-  ip.textContent = data.ip;
+  ipParagraph.textContent = data.ip;
   locations.textContent = `${data.location.city}, ${data.location.region} ${data.location.postalCode}`;
   timezone.textContent = "UTC " + data.location.timezone;
   isp.textContent = data.isp;
-  //console.log(data);
 };
-
-const actualizarMapa = () => {
+const actualizarMapa = (data) => {
+  lat = data.location.lat;
+  lng = data.location.lng;
   map.setView([lat, lng], 13);
 };
 var map = L.map("map").setView([37.38605, -122.08385], 13);
